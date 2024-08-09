18cc704094bf6014ad8214f6f2de2c14971c8edc
Harun Erdoğan <66188085+hrn-erdgn@users.noreply.github.com>
2024-08-09 15:34:10 +0300

    heatmap.py güncellendi
    Gereksiz numpy kütüphanesi kaldırıldı.
    Yorum satırları eklendi.

--- heatmap.py
+++ heatmap.py
@@ -1,28 +1,22 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-import yfinance as yf
+import pandas as pd                        # Veri işleme için pandas kütüphanesini import ediyoruz.
+import seaborn as sns                      # Korelasyon heatmap'i oluşturmak için seaborn kütüphanesini import ediyoruz.
+import matplotlib.pyplot as plt            # Grafik çizimlerini yapabilmek için matplotlib kütüphanesini import ediyoruz.
+from datetime import datetime, timedelta   # Dinamik günleri çekmek için
+import yfinance as yf                      # Yahoo Finance API'sini Python'da kullanmak için yfinance kütüphanesini import ediyoruz. Kapanış verileri için
 
 
 
 
 
 
-# Hisse senedi verilerini içeren bir DataFrame oluşturduğunuzu varsayalım.
-# Örnek olarak random verilerle gösterim yapıyorum.
-# Normalde BIST 30 hisselerinin tarihsel fiyatlarını bu DataFrame'e yüklemelisiniz.
+secim = input("BIST30 heatmapi gelsin istiyorsaniz 1, Hisseleri kendiniz girmek istiyorsaniz 2 girin. [Varsayilan 1] (1/2) : ") # Kullanıcıya Seçim Sunuyoruz
 
 
-secim = input("BIST30 heatmapi gelsin istiyorsaniz 1, Hisseleri kendiniz girmek istiyorsaniz 2 girin. [Varsayilan 1] (1/2) : ")
+if secim.lower() == "2":    # Seçin 2 ise girilen hisseleri parse ediyoruz
+    hisseler = input("Lütfen hisse senedi isimlerini ',' ile ayırarak girin (Örn: GARAN, AKBNK): ").split(',') # hisseleri parse ediyoruz
+    hisseler = [hisse.strip().upper() + ".IS" for hisse in hisseler] # Girilen hisselerin sonuna .IS ekliyoruz yahoo finance tickerları örn GARAN.IS olarak girmemizi istiyor
 
-
-if secim.lower() == "2":
-    hisseler = input("Lütfen hisse senedi isimlerini ',' ile ayırarak girin (Örn: GARAN, AKBNK): ").split(',')
-    hisseler = [hisse.strip().upper() + ".IS" for hisse in hisseler]
-
-else:
+else:                    # Varsayılan olarak BIST30 Tickerlarımız
     hisseler = [
     "AKBNK.IS", "ALARK.IS", "ASELS.IS", "ASTOR.IS", "BIMAS.IS",
     "BRSAN.IS", "DOAS.IS" , "EKGYO.IS", "ENKAI.IS", "EREGL.IS",
@@ -32,37 +26,37 @@ else:
     "TCELL.IS", "THYAO.IS", "TOASO.IS", "TUPRS.IS", "YKBNK.IS"
     ]
 
-hisseler.extend(["XU030.IS", "XU100.IS"])
+hisseler.extend(["XU030.IS", "XU100.IS"])        # Korelasyon ısı haritasına bist30 ve bist 100 endekslerinide ekliyoruz.
 
 
 
 
-gun = input("Kaç günlük veri almak istiyorsunuz? (Varsayılan: 365 gün): ")
+gun = input("Kaç günlük veri almak istiyorsunuz? (Varsayılan: 365 gün): ") # kaç günlük veri alınsın kullanıcıya soruyoruz
 
 
 if not gun.isdigit():
-    gun = 365
+    gun = 365         # Varsayılan olarak 365
 else:
     gun = int(gun)
 
 
-baslangic = (datetime.today() - timedelta(days=gun)).strftime('%Y-%m-%d')
-bugun = datetime.today().strftime('%Y-%m-%d')
+baslangic = (datetime.today() - timedelta(days=gun)).strftime('%Y-%m-%d')    # Girilen gün kadar geriye gidiyoruz.
+bugun = datetime.today().strftime('%Y-%m-%d') # Bugünü alıyoruz
 
 
 
-data = yf.download(hisseler, start=baslangic, end=bugun)['Adj Close']
+data = yf.download(hisseler, start=baslangic, end=bugun)['Adj Close']    # Yahoo finance'den hisseler listemizdeki hisselerin kapanış verilerini alıyoruz.
 
 # Korelasyon matrisini hesapla
 correlation_matrix = data.corr()
 
 
 
-ordered_cols = ["XU030.IS", "XU100.IS"] + [col for col in correlation_matrix.columns if col not in ["XU030.IS", "XU100.IS"]]
-correlation_matrix = correlation_matrix[ordered_cols].loc[ordered_cols]
+ordered_cols = ["XU030.IS", "XU100.IS"] + [col for col in correlation_matrix.columns if col not in ["XU030.IS", "XU100.IS"]] # Bist30  ve bist100 hisselerini listenin başına alıyoruz
+correlation_matrix = correlation_matrix[ordered_cols].loc[ordered_cols] #
 
 
-correlation_matrix.columns = [col.replace(".IS", "") for col in correlation_matrix.columns]
+correlation_matrix.columns = [col.replace(".IS", "") for col in correlation_matrix.columns]    # Tickerlardan .IS kısmını siliyoruz
 correlation_matrix.index = [index.replace(".IS", "") for index in correlation_matrix.index]
 
 # Heatmap'i oluştur
